dc8a93620649615504d5c8acefe9cc619f584d69
Thomas Green <thomaspetergreen@gmail.com>
2018-11-29 18:41:05 +1100

    Update links

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,9 +26,9 @@ const AboutPage = () => (
   <Layout>
     <About>
       <p>I'm a digital product and interaction designer in Melbourne, Australia. I’m interested in designing products, services and systems which empower individuals and create healthier communities.</p>
-      <p>I’ve worked with organisations operating in healthcare, education, government and the arts. My full work history is on <a href="#0">Linkedin</a>.</p>
-      <p>This site was built with <a href="#0">Gatsby</a> the source is available on <a href="#0">Github</a>. I take <Link to="notes">notes for future reference</Link> and save what I'm reading on <a href="https://www.goodreads.com/user/show/3196623-thomas-green">Goodreads</a>.</p>
-      <p><strong>Want to work together?</strong> <a href="#0">Send an email</a>&nbsp;&rarr;</p>
+      <p>I’ve worked with organisations operating in healthcare, education, government and the arts. My full work history is on <a href="https://www.linkedin.com/in/greenthoms" target="_blank" rel="noopener noreferrer">Linkedin</a>.</p>
+      <p>This site was built with <a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer">Gatsby</a> the source is available on <a href="https://github.com" target="_blank" rel="noopener noreferrer">Github</a>. I take <Link to="notes">notes for future reference</Link> and save what I'm reading on <a href="https://www.goodreads.com/user/show/3196623-thomas-green" target="_blank" rel="noopener noreferrer">Goodreads</a>.</p>
+      <p><strong>Want to work together?</strong> <a href="mailto:thomasgreen@fastmail.com" target="_blank" rel="noopener noreferrer">Send an email</a>&nbsp;&rarr;</p>
     </About>
   </Layout>
 )
